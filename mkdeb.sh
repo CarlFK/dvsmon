33ff8d8e45d4814e8a6bc96aea779c099a692fc5
Carl Karsten <carl@gw42.(none)>
2009-08-28 22:02:25 -0500

    deps and stuff

--- mkdeb.sh
+++ mkdeb.sh
@@ -2,7 +2,7 @@
 rm ../*.changes
 export DEBFULLNAME="Carl Karsten"
 export DEBEMAIL=carl@personnelware.com
-debchange --increment $0
+debchange --increment $1
 debuild -S -sa 
 cd ..
 dput CarlFK-ppa *.changes
