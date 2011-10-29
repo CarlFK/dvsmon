fdc8c63cf70ad373d0a836a0967a60a70d4325db
Carl Karsten <carl@personnelware.com>
2011-10-28 20:22:02 -0500

    mark run/kill/die events in logs

--- dvs-mon.py
+++ dvs-mon.py
@@ -17,108 +17,112 @@ class CommandRunner(object):
     """
 
     timerCallbacks = []
-    pid=None
+    pid = None
+    process = None
     
-    def __init__(self, cmd, parent):
+    def __init__(self, cmd, pain):
 
         self.cmd = cmd
-        self.process = None
         
         # outer panel to hold the 3 parts:
-        #  cmd+ctrls, stdout, stderr
-        self.panel_out = sc.SizedPanel(parent)
-        self.panel_out.SetSizerType('vertical')
-        self.panel_out.SetSizerProps(expand=True)
+        #  cmd+buttons, stdout, stderr
+        panel_cr = sc.SizedPanel(pain)
+        panel_cr.SetSizerType('vertical')
+        panel_cr.SetSizerProps(proportion=1, expand=True)
+        panel_cr.Bind(wx.EVT_END_PROCESS, self.ProcessEnded)
+        self.panel_cr = panel_cr
 
-        # self.panel = sc.SizedPanel(parent)
-        self.panel = sc.SizedPanel(self.panel_out)
-        self.panel.SetSizerType('horizontal')
-        self.panel.SetSizerProps(expand=True)
-        self.panel.Bind(wx.EVT_END_PROCESS, self.ProcessEnded)
+        # cmd+buttons
+        panel_cmd = sc.SizedPanel(panel_cr)
+        panel_cmd.SetSizerType('horizontal')
+        panel_cmd.SetSizerProps(expand=True)
 
-        self.txt_cmd = wx.TextCtrl(self.panel, value=self.cmd, style=wx.TE_READONLY)
-        self.txt_cmd.SetSizerProps(proportion=40, expand=True)
+        self.txt_cmd = wx.TextCtrl(
+                panel_cmd, value=self.cmd, style=wx.TE_READONLY)
+        self.txt_cmd.SetSizerProps(proportion=1, expand=True)
         self.txt_cmd.SetForegroundColour(wx.BLUE)
 
-        btn1 = wx.Button(self.panel, label='Run', size=(45, -1))
+        btn1 = wx.Button(panel_cmd, label='Run', size=(45, -1))
         btn1.Bind(wx.EVT_BUTTON, self.RunCmd)
 
-        btn2 = wx.Button(self.panel, label='Kill', size=(45,-1))
+        btn2 = wx.Button(panel_cmd, label='Kill', size=(45,-1))
         btn2.Bind(wx.EVT_BUTTON, self.Kill)
         
-        btn3 = wx.Button(self.panel, label='X', size=(25,-1))
+        btn3 = wx.Button(panel_cmd, label='X', size=(25,-1))
         btn3.Bind(wx.EVT_BUTTON, self.RemovePanel)
         
-        # self.panel2 = sc.SizedPanel(self.panel)
-        # self.panel2 = sc.SizedPanel(parent)
-        # self.panel2.SetSizerType('vertical')
-        # self.panel2.SetSizerProps(expand=True, proportion=2)
-        # self.stdout = wx.TextCtrl( self.panel2, style=wx.TE_READONLY|wx.TE_MULTILINE)
+        # sdtout
+        stdout = wx.TextCtrl( 
+                panel_cr, style=wx.TE_READONLY|wx.TE_MULTILINE)
+        stdout.SetSizerProps(proportion=1, expand=True)
+        self.stdout = stdout
 
-        self.stdout = wx.TextCtrl( self.panel_out, style=wx.TE_READONLY|wx.TE_MULTILINE)
-        self.stdout.SetSizerProps(proportion=1, expand=True)
-
-        # self.stderr = wx.TextCtrl(self.panel2, style=wx.TE_READONLY|wx.TE_MULTILINE)
-        self.stderr = wx.TextCtrl( self.panel_out, style=wx.TE_READONLY|wx.TE_MULTILINE)
-        self.stderr.SetSizerProps(proportion=1, expand=True)
-        self.stderr.SetForegroundColour(wx.RED)
+        # stderr
+        stderr = wx.TextCtrl( 
+                panel_cr, style=wx.TE_READONLY|wx.TE_MULTILINE)
+        stderr.SetSizerProps(proportion=1, expand=True)
+        stderr.SetForegroundColour(wx.RED)
+        self.stderr = stderr
 
         # add to the timer callback list:
         self.timerCallbacks.append(self.ShowIO)
         
+    def AppendLine(self, ctrl, line):
+        ctrl.AppendText("\n"+line)
+
+    def MarkOutErr(self, line):
+        self.AppendLine(self.stdout,line)
+        self.AppendLine(self.stderr,line)
+
     def RunCmd(self, event):
         if self.pid is None:
+            self.MarkOutErr("Starting...")
             self.txt_cmd.SetForegroundColour(wx.GREEN)
-            self.process = wx.Process(self.panel)
+            self.process = wx.Process(self.panel_cr)
             self.process.Redirect()
-            self.pid = wx.Execute(self.cmd, wx.EXEC_ASYNC, self.process)
+            self.pid = wx.Execute(
+                    self.cmd, wx.EXEC_ASYNC, self.process)
             print 'Executed:  %s' % (self.cmd)
             
     def Kill(self, event):
         if self.pid:
+            self.MarkOutErr("Killing...")
             print 'Killing: %s' % (self.cmd)
             wx.Process.Kill(self.pid)
             self.pid = None
             self.txt_cmd.SetForegroundColour(wx.BLUE)
-        
            
     def ShowIO(self):
+
         if self.process is not None:
 
-            stream = self.process.GetInputStream()
-            while stream.CanRead():
+            def one(stream,ctrl):
+              while stream.CanRead():
                 line = stream.read()
                 line = line.strip()
-                # print "O", line.__repr__()
-                self.stdout.AppendText("\n"+line)
+                self.AppendLine(ctrl,line)
 
-            stream = self.process.GetErrorStream()
-            while stream.CanRead():
-                line = stream.read()
-                line = line.strip()
-                # print "E", line.__repr__()
-                self.stderr.AppendText("\n"+line)
-
-    def RemovePanel(self, event):
-        # remove this command to make room for the stuff we care about
-        if self.pid is None:
-            parent=self.panel.GetTopLevelParent() # Parent
-            self.panel.Destroy() 
-            self.stdout.Destroy() 
-            self.stderr.Destroy() 
-            self.panel_out.Destroy() 
-            parent.SendSizeEvent()
+            one(self.process.GetInputStream(), self.stdout)
+            one(self.process.GetErrorStream(), self.stderr)
  
     def ProcessEnded(self, event):
         if self.pid:
             self.txt_cmd.SetForegroundColour(wx.RED)
             self.ShowIO()
+            self.MarkOutErr("DIED!")
 
         self.process.Destroy()
         self.process = None
 
         print 'Process %s terminated: %s' % (self.pid, self.cmd)
         self.pid=None
+
+    def RemovePanel(self, event):
+        # remove this command to make room for the stuff we care about
+        if self.pid is None:
+            parent=self.panel_cr.GetTopLevelParent() 
+            self.panel_cr.Destroy() 
+            parent.SendSizeEvent()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='DVswitch manager.')
@@ -142,8 +146,18 @@ def mk_commands(args):
     # where I use execfile() - it works.
     COMMANDS = [ ]
 
-    for cmd_file in args.commands:
-        execfile(cmd_file, locals())
+    if args.commands:
+        for cmd_file in args.commands:
+            execfile(cmd_file, locals())
+    else:
+        # for testing.
+        COMMANDS+=[ 
+            'ping -i .3 127.0.0.1',
+            # 'ping -i 1 127.0.0.1',
+            # 'ping localhost',
+            'ping -c 5 localhost',
+        ]
+
 
     # strip trailing spaces which get passed as a parameter.
     commands = [cmd.strip() for cmd in COMMANDS]
@@ -158,11 +172,12 @@ def main():
     app = wx.PySimpleApp()
     
     size=wx.GetDisplaySize()
-    frame = sc.SizedFrame(None, title='dvs-mon',  pos=(1,1), size=(450, size[1]))
-    panel = frame.GetContentsPane()
+    frame = sc.SizedFrame(
+            None, title='dvs-mon',  pos=(1,1), size=(450, size[1]-100))
+    pain = frame.GetContentsPane()
 
     for cmd in commands:
-        cr = CommandRunner(cmd, panel)
+        cr = CommandRunner( cmd, pain )
 
     frame.Show()
 
@@ -170,9 +185,9 @@ def main():
         for cb in CommandRunner.timerCallbacks:
             cb()
 
-    panel.Bind(wx.EVT_TIMER, OnTimer)
-    timer = wx.Timer(panel)
-    timer.Start(1000)
+    pain.Bind(wx.EVT_TIMER, OnTimer)
+    timer = wx.Timer( pain )
+    timer.Start(100)
 
     app.MainLoop()
 
--- test_ping.py
+++ test_ping.py
@@ -2,8 +2,8 @@
 
 COMMANDS+=[ 
     'ping -i .3 127.0.0.1',
-    'ping -i 1 127.0.0.1',
-    'ping localhost',
+    # 'ping -i 1 127.0.0.1',
+    # 'ping localhost',
     'ping -c 5 localhost',
 ]
 
