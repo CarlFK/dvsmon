288ee87923a9855ba96ae62866f073b6d12cbf08
Tim 'mithro' Ansell <mithro@mithis.com>
2012-08-09 20:00:18 +1000

    Trying to fix Issue #3.
    This create a seperate thread per process which uses epoll to watch
    stderr/stdout and append them to the controls. We also limit the amount of data
    which will appear in the control.

--- bug_1.sh
+++ bug_1.sh
@@ -3,11 +3,11 @@
 # bug_1.sh
 # demos the blocking bug.  at least I think it is blocking.
 
-/usr/bin/python2.7 ./dvs-mon.py \
+/usr/bin/python ./dvs-mon.py \
   --host localhost --port 2000 \
   --commands \
     dvswitch.py \
-    source_alsa.py \
+    test_spew.py \
   --keepalive 100 \
   --show-all-detail \
     $*
