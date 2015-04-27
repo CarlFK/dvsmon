eedc1dcccf2ca4453d696b9da833e19e05d13947
Carl Karsten <carl@personnelware.com>
2015-04-27 13:15:49 -0500

    define video params so that server and clients use the same.

--- mixy.py
+++ mixy.py
@@ -1,8 +1,14 @@
 # gswitch.py - builds list of COMMANDS to run gst-switch components 
 
+# copied from run-demo.sh
+VIDEO_CAPS="video/x-raw, format=(string)I420, pixel-aspect-ratio=(fraction)1/1, width=(int)300, height=(int)200, framerate=(fraction)25/1"
+AUDIO_CAPS="audio/x-raw, rate=48000, channels=2, format=S16LE, layout=interleaved"
+
+# VIDEO_CAPS = VIDEO_CAPS.replace("(","\(").replace(")","\)")
+
 # COMMANDS.append( Command('gst-switch-srv --record foo') )
 COMMANDS.append( Command(
-    'gst-switch-srv --record foo.avi') )
+    'gst-switch-srv --record foo.avi -f "{}"'.format(VIDEO_CAPS)) )
     # 'gst-switch-srv --record %Y-%m-%dT%H_%M_%S.avi') )
 
 COMMANDS.append( Command('gst-switch-ui') )
@@ -10,7 +16,7 @@ COMMANDS.append( Command('gst-switch-ui') )
 vid_src = """
 gst-launch-1.0 
     videotestsrc pattern=18 is-live=1 
-    ! video/x-raw, width=300, height=200 
+    ! "{}"
     ! timeoverlay font-desc="Sans 40" 
     ! clockoverlay time-format="%S" font-desc="Sans 240" 
     ! textoverlay text="s-{}" 
@@ -19,9 +25,9 @@ gst-launch-1.0
     ! tcpclientsink port=3000 
 """.replace('\n','')
 
-COMMANDS.append( Command(vid_src.format(1)) )
-COMMANDS.append( Command(vid_src.format(2)) )
-COMMANDS.append( Command(vid_src.format(3)) )
+COMMANDS.append( Command(vid_src.format(VIDEO_CAPS,1)) )
+COMMANDS.append( Command(vid_src.format(VIDEO_CAPS,2)) )
+COMMANDS.append( Command(vid_src.format(VIDEO_CAPS,3)) )
 
 src = """
 gst-launch-1.0 
