5b83df925f27bcba25be545698d13ceca921e3b6
Carl Karsten <carl@personnelware.com>
2015-04-27 13:33:14 -0500

    set audio parameters in the test client. The server is hardcoded and does not take a parameter.

--- mixy.py
+++ mixy.py
@@ -4,8 +4,6 @@
 VIDEO_CAPS="video/x-raw, format=(string)I420, pixel-aspect-ratio=(fraction)1/1, width=(int)300, height=(int)200, framerate=(fraction)25/1"
 AUDIO_CAPS="audio/x-raw, rate=48000, channels=2, format=S16LE, layout=interleaved"
 
-# VIDEO_CAPS = VIDEO_CAPS.replace("(","\(").replace(")","\)")
-
 # COMMANDS.append( Command('gst-switch-srv --record foo') )
 COMMANDS.append( Command(
     'gst-switch-srv --record foo.avi -f "{}"'.format(VIDEO_CAPS)) )
@@ -32,14 +30,15 @@ COMMANDS.append( Command(vid_src.format(VIDEO_CAPS,3)) )
 src = """
 gst-launch-1.0 
     audiotestsrc
+    ! "{}"
     ! queue 
     ! audioconvert
     ! gdppay 
     ! tcpclientsink port=4000 
 """.replace('\n','')
 
-COMMANDS.append( Command(src) )
-COMMANDS.append( Command(src) )
+COMMANDS.append( Command(src.format( AUDIO_CAPS)) )
+COMMANDS.append( Command(src.format( AUDIO_CAPS)) )
 
 vid_src = """
 gst-launch-1.0 v4l2src device=/dev/video0 \
