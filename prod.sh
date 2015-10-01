60e485b5fab6143948dd862c2057ab51bdcf46ad
Carl Karsten <carl@personnelware.com>
2015-10-01 06:44:09 +0200

    -c image/jpeg for hdmi2usb

--- prod.sh
+++ prod.sh
@@ -5,8 +5,8 @@
 ./dvs-mon.py -c \
     dvswitch.py \
     source_fw.py \
+    source_remote_usb.py \
     source_remote_fw.py \
-    sink_find_dir.py \
-    sink_ffplay.py \
+    sink_find_dir.py 
  
 # --host 130.216.0.$1 --port 1234
