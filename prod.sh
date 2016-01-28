a3297ab618e255950bd45d68475c129688f7ba76
Carl Karsten <carl@personnelware.com>
2016-01-28 21:47:14 +1100

    order production components sanely.

--- prod.sh
+++ prod.sh
@@ -7,10 +7,10 @@ cd $(dirname $0)
 ./dvs-mon.py -c \
     dvswitch-schroot.py \
     source_alsa.py \
-    source_fw.py \
-    source_remote_usb.py \
     source_remote_fw.py \
+    source_remote_usb.py \
     sink_find_dir.py 
 
 #  dvswitch.py \
+#    source_fw.py \
 # --host 130.216.0.$1 --port 1234
