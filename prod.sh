ecf42645eeeceea7cb442126594f943ccde176a3
Carl Karsten <carl@personnelware.com>
2016-04-27 09:33:25 -0500

    change default vocto dir

--- prod.sh
+++ prod.sh
@@ -2,10 +2,13 @@
 
 # sencible params for production (recording talks) 
 
+# $1 is the dir voctomix is in.  defaults to the dir dvsmon is in.
+
 cd $(dirname $0)
 dvs_dir=$PWD
 
-vocto_dir="${1:-~/voctomix}"
+cd ..
+vocto_dir="${1:-voctomix}"
 cd $vocto_dir
 
 $dvs_dir/dvs-mon.py -c \
